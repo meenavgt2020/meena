f3b5e0af1f2c68ee89d0a731aab865a53b2f8480
meenakshi <42331796+meenavgt2020@users.noreply.github.com>
2018-09-03 12:13:13 +0530

    form2nd_page

---
+++ 2nd.php
@@ -0,0 +1,19 @@
+<?php 
+$yourname=$_POST['Name'];
+print "yourname: $yourname<br>";
+$yourmail=$_POST['mail'];
+echo "yourmail: $yourmail<br>";
+$yourreg=$_POST['register'];
+echo "yourreg: $yourreg<br>";
+$yourage=$_POST['age'];
+echo "yourage: $yourage<br>";
+$gender=$_POST['gender'];
+echo "gender: $gender<br>";
+$yourgames=$_POST['games'];
+echo "yourgames: $yourgames<br>";
+$yourrating=$_POST['rating'];
+echo "yourreg: $yourrating<br>";
+$yourtext=$_POST['textarea1'];
+echo "yourtext: $yourtext<br>";
+
+?>
